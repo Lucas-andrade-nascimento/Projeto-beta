2d3d024880327ec9e39bb1fec383c3ccdac7cd75
Lucas Andrade <andradedesa.lucas@gmail.com>
2022-04-04 08:53:49 -0300

    first update

---
+++ js/funcoes.js
@@ -0,0 +1,9 @@
+var nome;
+nome = prompt("Ola, informa seu nome","seu nome aqui");
+// criando a função em JS
+function mensagem() {
+    alert('Olá um super bom dia!');
+}
+function vindas (){
+    alert("Seja bem vindo "+nome);
+}
